ad9d21a2aaf97f8c2a6009773d688679d9ecabec
Max Kulicke <mkulicke@gmail.com>
2019-12-14 15:00:37 -0500

    got clock successfully initialized, created a setInterval to display/update the clock

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var now = moment();
-$("#clock").text(now);
+setInterval(function () {
+  // run every 60 seconds instead of every second?
+  var now = moment().format('hh:mm:ss: A');
+  $("#clock").text(now);
+}, 1000);
+
+// TEST JSON area
+var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+// Put the object into storage
+console.log(JSON.stringify(testObject));
+localStorage.setItem('testObject', JSON.stringify(testObject));
+
+// Retrieve the object from storage
+var retrievedObject = localStorage.getItem('testObject');
+
+console.log('retrievedObject: ', JSON.parse(retrievedObject));
