aaee11b1a2498b7164c2a48440899dcac0b0e1ee
Max Kulicke <mkulicke@gmail.com>
2019-12-14 16:13:26 -0500

    created dummy functions to begin mapping the flow out

--- script.js
+++ script.js
@@ -4,6 +4,42 @@ setInterval(function () {
   $("#clock").text(now);
 }, 1000);
 
+// will create all of the HR objects
+function createObjects() {
+
+}
+
+// storage function, pushes obj array (days) to local storage
+function storage(days) {
+
+}
+
+// checks the time, calls update every minute?
+function checkTime(time) {
+
+}
+
+// will call pastPresentFuture(), display(); storage()
+function timeUpdate() {
+
+}
+
+// will check and possibly update the past present future keys of the day obj
+function pastPresentFuture(day) {
+
+}
+
+// will update the front end (somehow?)
+function display() {
+
+}
+
+// will add user form input string (event) to day obj [] of strings
+function push(day, event) {
+
+}
+
+
 // TEST JSON area
 var testObject = { 'one': 1, 'two': 2, 'three': 3 };
 
