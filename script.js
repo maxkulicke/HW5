a9854f41085daffa8b324518e4bca4314d0de95c
Max Kulicke <mkulicke@gmail.com>
2019-12-14 16:57:12 -0500

    wrote a bunch of functions out, haven't tested many of them, still need to write storage() and display()

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
+// global variables
+var day = [];
+
 setInterval(function () {
   // run every 60 seconds instead of every second?
-  var now = moment().format('hh:mm:ss: A');
+  var now = getNow();
   $("#clock").text(now);
+  checkTime();
+  display(day);
+  return now;
 }, 1000);
+
+// TODO
+// onclick function for edit button
+// onclick function for update button
+
+// returns the current time (string type)
+function getNow() {
+  var now = moment().format('HH:mm:ss: A');
+  return now;
+}
+
+// returns the current hour (09 - 17) (int type)
+function getHour() {
+  var now = getNow();
+  var hour = parseInt(now.charAt(0) + now.charAt(1));
+  return hour;
+}
 
 // will create all of the HR objects
 function createObjects() {
-
+  // get HR in here?
+  for (var i = 9; i < 17; i++) {
+    var hour = { 
+      slot: i, 
+      past: false, 
+      present: false, 
+      future: false, 
+      events: [],
+    }
+    day.push(hour);
+  }
+  pastPresentFuture(day);
 }
 
 // storage function, pushes obj array (days) to local storage
@@ -16,27 +50,44 @@ function storage(days) {
 
 // checks the time, calls update every minute?
 function checkTime(time) {
-
+  var now = getNow;
+  var second = parseInt(now.charAt(6) + now.charAt(7));
+  if (second == 0) {
+    timeUpdate();
+  }
 }
 
 // will call pastPresentFuture(), display(); storage()
 function timeUpdate() {
-
+  for (var i = 0; i < day.length; i++) {
+    pastPresentFuture(day[i]);
+    storage(day);
+    display(day); // is this call needed?
+  }
 }
 
 // will check and possibly update the past present future keys of the day obj
 function pastPresentFuture(day) {
-
+  var hourNow = getHour();
+  if (hour.slot < hourNow) {
+    hour.past = true;
+  }
+  else if (hour.slot > hourNow) {
+    hour.future = true;
+  }
+  else {
+    hour.present = true;
+  }
 }
 
 // will update the front end (somehow?)
-function display() {
+function display(day) {
 
 }
 
-// will add user form input string (event) to day obj [] of strings
-function push(day, event) {
-
+// will add user form input string (event) to hour obj [] of strings
+function push(hour, event) {
+  hour.events.push(event);
 }
 
 
